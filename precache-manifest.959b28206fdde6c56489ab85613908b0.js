55f06bbc9f4d4c3ca737b364f6c3b153ec374035
alysialfi <9c.alysialfi@gmail.com>
2020-10-30 00:30:46 +0700

    Updates

---
+++ precache-manifest.959b28206fdde6c56489ab85613908b0.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bff4598fd5845494fe33e41e907c756",
+    "url": "/PersonalWeb/index.html"
+  },
+  {
+    "revision": "173ed1032b42caf05211",
+    "url": "/PersonalWeb/static/css/2.2944f1bf.chunk.css"
+  },
+  {
+    "revision": "262ae211bb5268a9159c",
+    "url": "/PersonalWeb/static/css/main.6a32a5bb.chunk.css"
+  },
+  {
+    "revision": "173ed1032b42caf05211",
+    "url": "/PersonalWeb/static/js/2.1460c066.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/PersonalWeb/static/js/2.1460c066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262ae211bb5268a9159c",
+    "url": "/PersonalWeb/static/js/main.0a1b447f.chunk.js"
+  },
+  {
+    "revision": "e7e2eea1078906717dd7",
+    "url": "/PersonalWeb/static/js/runtime-main.89c3451c.js"
+  },
+  {
+    "revision": "0aaad096cd13b7b002577f2708057894",
+    "url": "/PersonalWeb/static/media/about_pict.0aaad096.png"
+  },
+  {
+    "revision": "2f972da39d6cfe3ce5405095c69b44d1",
+    "url": "/PersonalWeb/static/media/ballon.2f972da3.png"
+  },
+  {
+    "revision": "c849d02a1538ec25c7ea04592272aced",
+    "url": "/PersonalWeb/static/media/bg_about.c849d02a.png"
+  },
+  {
+    "revision": "62cef77b9df91e4f6a0fa9e7957815e4",
+    "url": "/PersonalWeb/static/media/bird_flying.62cef77b.png"
+  },
+  {
+    "revision": "05bdb0710c589ce8d8e2ca8f3e9b08ab",
+    "url": "/PersonalWeb/static/media/circle.05bdb071.png"
+  },
+  {
+    "revision": "09177342fd5f042c33e08e4b0cdffd9b",
+    "url": "/PersonalWeb/static/media/cloud.09177342.png"
+  },
+  {
+    "revision": "fbfc0670a2a39ea3befc6996188545c1",
+    "url": "/PersonalWeb/static/media/coding.fbfc0670.png"
+  },
+  {
+    "revision": "9eaa1a96f28e268bf00a5d6f88ac42c1",
+    "url": "/PersonalWeb/static/media/divider2.9eaa1a96.png"
+  },
+  {
+    "revision": "ad991a7cd81a46231761018a729830cb",
+    "url": "/PersonalWeb/static/media/divider_white.ad991a7c.png"
+  },
+  {
+    "revision": "f6b31344f5a7af8c38b921f06eb7c9de",
+    "url": "/PersonalWeb/static/media/footer_pict.f6b31344.png"
+  },
+  {
+    "revision": "5a9476afec7f484e38df7cd2ab0af3ed",
+    "url": "/PersonalWeb/static/media/foto_alysia.5a9476af.jpg"
+  },
+  {
+    "revision": "8245e240600a88d96f0b0fe9a76506bd",
+    "url": "/PersonalWeb/static/media/github2.8245e240.svg"
+  },
+  {
+    "revision": "706ede8e458587e91c19dab02736cd7f",
+    "url": "/PersonalWeb/static/media/gmail2.706ede8e.svg"
+  },
+  {
+    "revision": "ac1183537036f00e8031843825a32045",
+    "url": "/PersonalWeb/static/media/houses.ac118353.png"
+  },
+  {
+    "revision": "d1c6d387867a6c755351beada95c2734",
+    "url": "/PersonalWeb/static/media/instagram2.d1c6d387.svg"
+  },
+  {
+    "revision": "634a53333f3cd273085c003882c5fd7e",
+    "url": "/PersonalWeb/static/media/linkedin2.634a5333.svg"
+  },
+  {
+    "revision": "62ba17d35b02a12a1913bee60116296f",
+    "url": "/PersonalWeb/static/media/me_logo.62ba17d3.png"
+  },
+  {
+    "revision": "faa414a2ed427635461bf242b23bd8ad",
+    "url": "/PersonalWeb/static/media/moon.faa414a2.png"
+  },
+  {
+    "revision": "df1ac8d12408087f3efc6dffbcdfaaad",
+    "url": "/PersonalWeb/static/media/nomu9_fix.df1ac8d1.mp4"
+  },
+  {
+    "revision": "6e903c22a53cd47b3b87f09a7a8adb1c",
+    "url": "/PersonalWeb/static/media/php.6e903c22.svg"
+  },
+  {
+    "revision": "5ab0937fc99383983a4e4af442f0dfb8",
+    "url": "/PersonalWeb/static/media/pole.5ab0937f.png"
+  },
+  {
+    "revision": "f8411563b2ac4b788b1eda92aa3715b3",
+    "url": "/PersonalWeb/static/media/porto1.f8411563.png"
+  },
+  {
+    "revision": "289e3e3c2e7c5492e6a5937cb6768e21",
+    "url": "/PersonalWeb/static/media/porto2.289e3e3c.png"
+  },
+  {
+    "revision": "18d53edf5e87e96dc980be0514489781",
+    "url": "/PersonalWeb/static/media/porto3.18d53edf.png"
+  },
+  {
+    "revision": "e75236d0c73a118df3bb8c8a1f9d3dd8",
+    "url": "/PersonalWeb/static/media/porto4.e75236d0.png"
+  },
+  {
+    "revision": "dcafec789ffddb661fac6470d6cd593e",
+    "url": "/PersonalWeb/static/media/sakura.dcafec78.png"
+  },
+  {
+    "revision": "6a976effdf80c70c8a02b3d6805e9842",
+    "url": "/PersonalWeb/static/media/sertif1.6a976eff.png"
+  },
+  {
+    "revision": "f756456673e61c14f270526e7f6ce7ae",
+    "url": "/PersonalWeb/static/media/sertif2.f7564566.png"
+  },
+  {
+    "revision": "91dfcf267d0cdf497e57ee5ab064a97a",
+    "url": "/PersonalWeb/static/media/sertif3.91dfcf26.png"
+  },
+  {
+    "revision": "79807441ce8c577755b18b6c8beba389",
+    "url": "/PersonalWeb/static/media/sertif4.79807441.png"
+  },
+  {
+    "revision": "34809190d19e399fe9dcd7d7c2747497",
+    "url": "/PersonalWeb/static/media/sertif5.34809190.png"
+  },
+  {
+    "revision": "ef4d667f69cad616167a8106dc7c8b64",
+    "url": "/PersonalWeb/static/media/sertif6.ef4d667f.png"
+  },
+  {
+    "revision": "8ccee7290ce12e87010ffb7e3381d350",
+    "url": "/PersonalWeb/static/media/up-arrow.8ccee729.svg"
+  },
+  {
+    "revision": "df7003b815aa9863e9fb607f829ac570",
+    "url": "/PersonalWeb/static/media/video.df7003b8.png"
+  },
+  {
+    "revision": "d91bfc858c0902cd0f9b5d1d08fd49b4",
+    "url": "/PersonalWeb/static/media/worldwide.d91bfc85.svg"
+  }
+]);
